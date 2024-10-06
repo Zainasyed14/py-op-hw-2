37012e3d5993f429829c85c379d8988f58315565
Zainasyed14 <zainasyed12345@gmail.com>
2024-10-06 04:36:46 -0700

    Co-authored-by: Shrey Mishra <shreymishra13@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 name =input("Enter your charcter : ")
-if name>"a" and name<"z" :
+
+
+# a-z or A-Z
+if (name>="a" and name<="z") or (name>="A" and name<="Z"):
     print("it is an alphabet")
-else 1>9 :
+else :
     print("it is a number")
 
